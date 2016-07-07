a87fbf1adce95c0747ac393a2f0415f7c01365c2
stevenay <naylinaung.softwarearchitect@gmail.com>
2016-07-07 12:40:10 +0630

    Project Created and Templated

---
+++ app/src/main/java/me/naylinaung/padc_myanmarattractions/data/models/AttractionModel.java
@@ -0,0 +1,7 @@
+package me.naylinaung.padc_myanmarattractions.data.models;
+
+/**
+ * Created by NayLinAung on 7/7/2016.
+ */
+public class AttractionModel {
+}
---
+++ app/src/main/java/me/naylinaung/padc_myanmarattractions/data/vos/AttractionVO.java
@@ -0,0 +1,7 @@
+package me.naylinaung.padc_myanmarattractions.data.vos;
+
+/**
+ * Created by NayLinAung on 7/7/2016.
+ */
+public class AttractionVO {
+}
